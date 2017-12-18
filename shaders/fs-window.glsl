b50c1f362fdd71fd770b87804cd29a8fd7bae668
cabbibo <isaaclandoncohen@gmail.com>
2017-12-18 02:08:01 -0800

    fixing shaders and start text

--- shaders/fs-window.glsl
+++ shaders/fs-window.glsl
@@ -12,6 +12,8 @@ varying vec3 vPos;
 $uvNormalMap
 $semLookup
 
+$simplex
+
 void main(){
 
 	vec3 fNorm = uvNormalMap( t_normal , vPos , vUv  , vNorm , 1.4 , 1.1 );
@@ -20,8 +22,10 @@ void main(){
   vec2 semLU = semLookup( normalize( vEye ) , vNorm );
   vec4 sem = texture2D( t_matcap , semLU );
 
+  float n = snoise( vec3( vPos.x * 200. , vPos.y  * 10. , vPos.z * 200.));
+
  // vec4 audio = texture2D( t_audio , vec2( lamb , 0. ));
-  gl_FragColor = vec4( vec3(length(sem.xyz * .3)), 1. );
+  gl_FragColor = vec4( vec3(length( abs(n) * sem.xyz * sem.xyz * .3)), 1. );
 
 
 }
